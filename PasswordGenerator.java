e3fc185b2e5d76cf7a3fdc4ecb078f7168ece682
Charlie Simpson <csimpson1230@gmail.com>
2017-07-26 00:14:26 +0000

    program has an error

---
+++ PasswordGenerator.java
@@ -0,0 +1,82 @@
+// This utility class can be used to generate random passwords.
+import java.util.Random;
+import java.util.Arrays;
+public class PasswordGenerator
+{
+    //class variables
+    private String password;
+
+    private char[] lowerAlpha = {'a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+            'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u','v',
+            'w', 'x' , 'y', 'z'};
+    private char[] upperAlpha = {'A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+            'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U','V',
+            'W', 'X' , 'Y', 'Z'};
+    private char[] passSymbol = {'!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-',
+            '+', '=', '{', '[', '}', ']', ';', ':', '<', ',', '>', '.', '?', '/',
+            '~'};
+    private char[] lowerLetter;
+    private int[] passDigit;
+
+    private int passSize;
+
+    // 0-arg constructor
+    public PasswordGenerator(){}
+
+    // 1-arg constructor
+    public PasswordGenerator(int passSize)
+    {
+        setPassSize(passSize);
+    }
+
+    //constructor (not sure how many args yet)
+    public PasswordGenerator(String password, char[] lowerAlpha, char[] upperAlpha,
+                             char[] passSymbol, int[] passDigit, int passSize)
+    {
+
+        this.password = password;
+        this.lowerAlpha = lowerAlpha;
+        this.upperAlpha = upperAlpha;
+        this.passSymbol = passSymbol;
+        this.passDigit = passDigit;
+        this.passSize = passSize;
+
+    }
+
+    public void setPassSize(int passSize)
+    {
+        // This checks to see if the password size being passed is not less than or equal to zero
+        if (passSize <= 0)
+            // If it is, do not set the passSize to less than or equal to zero, so return.
+            return;
+
+        else
+            this.passSize = passSize;
+
+    }
+    public int getPassSize()
+    {
+        return this.passSize = passSize;
+    }
+
+
+
+    public void setRandomLowerAlpha(char[] lowerAlpha, int passSize)
+    {
+        int size = passSize;
+        char[] lowerCase = new char[size];
+
+        Random r = new Random();
+        for (int i = 0; i < size; i++ )
+        {
+            lowerCase[i] = lowerAlpha[r.nextInt(26)];
+               
+        }
+        System.out.println(lowerCase);
+    }
+
+   
+    
+
+
+}
---
+++ PasswordGeneratorTester.java
@@ -0,0 +1,81 @@
+// This is the PasswordGenerator classes tester, this file is designed to fully excercise
+// The PasswordGenerator class so that we can test every feature.
+
+import java.util.Scanner;
+import java.util.Random;
+import java.util.Arrays;
+
+public class PasswordGeneratorTester {
+
+    public static void main(String[] args)
+    {
+        //variables
+        int size = 0;
+        String needSymbol = " ";
+        
+        //boolean Symbol = false;
+
+        //scanner
+        Scanner input = new Scanner(System.in);
+
+        size = passwordSize(size); // gets password size
+
+        PasswordGenerator pass = new PasswordGenerator(); // creates password object
+
+        pass.setPassSize(size);
+        System.out.println(pass.getPassSize());
+
+        needSymbol = symbolNeeded(needSymbol);
+        System.out.println(needSymbol);
+
+        pass.setRandomLowerAlpha(size);
+        
+
+
+    }
+    // input for size of password needed
+    public static int passwordSize(int size)
+    {
+        Scanner input = new Scanner(System.in);
+        //password length
+        System.out.print("Enter the length of password needed: ");
+        size = input.nextInt();
+        while(size <= 0)
+        {
+            System.out.print("Enter the length of password needed: ");
+            size = input.nextInt();
+        }
+        return size;
+    }
+
+    //input for pass needs symbol
+    public static String symbolNeeded(String needSymbol)
+    {
+        //boolean answer = false;
+        Scanner input = new Scanner(System.in);
+        //prompt for needing symbol
+        System.out.print("Does your password need a symbol? yes or no: ");
+        needSymbol = input.next();
+
+        while(true)
+        {
+            if(needSymbol.equals("yes"))
+            {
+                //answer = true;
+                return needSymbol; //break;
+            }
+            else if(needSymbol.equals("no"))
+            {
+                //answer = true;
+                return needSymbol; //break;
+            }
+            else
+            {
+                System.out.print("Does your password need a symbol? yes or no: ");
+                needSymbol = input.next();
+            }
+
+        }
+        //return needSymbol; //not sure if this return is staying or not
+    }
+}
